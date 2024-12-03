ac94181820a7a5f0a91b69f55120d153af01efc3
Moskas <minemoskas@gmail.com>
2024-12-03 20:03:38 +0100

    Day 3 Solution Part II

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,61 +1,38 @@
 use regex::Regex;
 use std::fs;
 
-fn get_input() -> Vec<String> {
-  fs::read_to_string("input.txt")
-    .expect("No input file provided")
-    .lines()
-    .map(|s| s.to_string())
-    .collect::<Vec<String>>()
+fn get_input() -> String {
+  fs::read_to_string("input.txt").expect("No input file provided")
 }
 
 fn main() {
-  let input = get_input().concat();
-  // Part I
-  let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-  let mut total = 0;
+  let input = get_input();
+
+  let re = Regex::new(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))").unwrap();
+
+  let mut total1 = 0;
+  let mut total2 = 0;
+  let mut enabled = true;
+
   for cap in re.captures_iter(&input) {
-    let num1: i32 = cap[1].parse().unwrap();
-    let num2: i32 = cap[2].parse().unwrap();
-    total += num1 * num2;
+    if cap.get(3).is_some() {
+      // Match "do()"
+      enabled = true;
+    } else if cap.get(4).is_some() {
+      // Match "don't()"
+      enabled = false;
+    } else if let (Some(a), Some(b)) = (cap.get(1), cap.get(2)) {
+      // Match "mul(a, b)"
+      let a: i32 = a.as_str().parse().unwrap();
+      let b: i32 = b.as_str().parse().unwrap();
+      let x = a * b;
+      total1 += x;
+      if enabled {
+        total2 += x;
+      }
+    }
   }
 
-  println!("Total result of mul instructions: {}", total);
-
-  // Part II
-  // let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-  // Regex to match the do() and don't() instructions
-  //let do_re = Regex::new(r"do\(\)").unwrap();
-  //let dont_re = Regex::new(r"don't\(\)").unwrap();
-
-  //let mut sum = 0;
-  //let mut mul_enabled = true; // mul instructions are enabled by default
-
-  //// Iterate over the entire input string
-  // let mut i = 0;
-  // while i < input.len() {
-  //   // Check for do() or don't() instructions
-  //   if let Some(cap) = do_re.find(&input[i..]) {
-  //     mul_enabled = true; // Enable multiplication
-  //     i += cap.end(); // Move past the "do()" instruction
-  //   } else if let Some(cap) = dont_re.find(&input[i..]) {
-  //     mul_enabled = false; // Disable multiplication
-  //     i += cap.end(); // Move past the "don't()" instruction
-  //   } else {
-  //     // Check for mul(X,Y) instructions
-  //     if let Some(cap) = re.captures(&input[i..]) {
-  //       if mul_enabled {
-  //         let x: i32 = cap[1].parse().unwrap();
-  //         let y: i32 = cap[2].parse().unwrap();
-  //         sum += x * y;
-  //       }
-  //       i += cap.get(0).unwrap().end(); // Skip over this mul instruction
-  //     } else {
-  //       i += 1; // Move to next character if no match
-  //     }
-  //   }
-  // }
-
-  //// Print the total sum
-  // println!("Total sum of enabled multiplications: {}", sum);
+  println!("Total result of mul instructions: {}", total1);
+  println!("Total result of only enabled mul instructions: {}", total2);
 }
